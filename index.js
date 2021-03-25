9f091685e41b76eec76499f682c51fa5371b044a
utkubgorgulu <79986859+utkubgorgulu@users.noreply.github.com>
2021-03-25 16:11:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 2) + 1; //1-6
+
+var randomDiceImage = "sila" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+if (randomNumber1 === 1) {
+  document.querySelector("h1").innerHTML = "Şişman"
+}
+else if (randomNumber1 === 2) {
+  document.querySelector("h1").innerHTML = "Aç"
+}
+else {
+  document.querySelector("h1").innerHTML = "Uykulu"
+}
